b7d31899dcc224cd2f9d4f43dd9eed909cbe400f
baoloc008 <baoloc008@gmail.com>
2020-11-03 21:37:21 +0700

    part2: autowired, primary, qualifier

---
+++ part2_autowired_primary_qualifier/src/main/java/com/demo/part2_autowired_primary_qualifier/Girl.java
@@ -0,0 +1,22 @@
+package com.demo.part2_autowired_primary_qualifier;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Girl {
+    // Đánh dấu để Spring inject một đối tượng Outfit vào đây
+    @Autowired
+    @Qualifier("bikini")
+    Outfit outfit;
+
+    // Spring sẽ inject outfit thông qua Constructor trước
+    public Girl() {
+    }
+
+    // Nếu không tìm thấy Constructor thoả mãn, nó sẽ thông qua setter
+    public void setOutfit(Outfit outfit) {
+        this.outfit = outfit;
+    }
+}
---
+++ part2_autowired_primary_qualifier/src/main/java/com/demo/part2_autowired_primary_qualifier/Outfit.java
@@ -0,0 +1,5 @@
+package com.demo.part2_autowired_primary_qualifier;
+
+public interface Outfit {
+    void wear();
+}
