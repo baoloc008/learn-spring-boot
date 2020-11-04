950da2d9c2994f11ceb2fb44f0f2f1c42c4b6a66
baoloc008 <baoloc008@gmail.com>
2020-11-04 16:31:17 +0700

    part5: component scan

---
+++ part5_component_scan/src/main/java/com/demo/part5_component_scan/Girl.java
@@ -0,0 +1,12 @@
+package com.demo.part5_component_scan;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Girl {
+
+    @Override
+    public String toString() {
+        return "Girl.java";
+    }
+}
---
+++ part5_component_scan/src/main/java/com/demo/part5_component_scan/others/OtherGirl.java
@@ -0,0 +1,11 @@
+package com.demo.part5_component_scan.others;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OtherGirl {
+    @Override
+    public String toString() {
+        return "OtherGirl.java";
+    }
+}
