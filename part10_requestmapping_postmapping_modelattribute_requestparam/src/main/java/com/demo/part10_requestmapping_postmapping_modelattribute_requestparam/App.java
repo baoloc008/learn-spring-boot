df2d5b7e887bb2afbc3f387a7c6b4d506267dcee
baoloc008 <baoloc008@gmail.com>
2020-11-05 22:40:09 +0700

    part10: requestmapping, postmapping, modelattribute, requestparam

---
+++ part10_requestmapping_postmapping_modelattribute_requestparam/src/main/java/com/demo/part10_requestmapping_postmapping_modelattribute_requestparam/App.java
@@ -0,0 +1,11 @@
+package com.demo.part10_requestmapping_postmapping_modelattribute_requestparam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ part10_requestmapping_postmapping_modelattribute_requestparam/src/main/java/com/demo/part10_requestmapping_postmapping_modelattribute_requestparam/Todo.java
@@ -0,0 +1,9 @@
+package com.demo.part10_requestmapping_postmapping_modelattribute_requestparam;
+
+import lombok.Data;
+
+@Data
+public class Todo {
+    public String title;
+    public String detail;
+}
