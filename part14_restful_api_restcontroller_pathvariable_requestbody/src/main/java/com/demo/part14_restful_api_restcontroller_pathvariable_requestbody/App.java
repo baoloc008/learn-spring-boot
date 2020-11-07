f85da8129f37e8c8f86e8777d5a35f36d33a5b70
baoloc008 <baoloc008@gmail.com>
2020-11-07 11:30:56 +0700

    part14: restful api, restcontroller, pathvariable, requestbody

---
+++ part14_restful_api_restcontroller_pathvariable_requestbody/src/main/java/com/demo/part14_restful_api_restcontroller_pathvariable_requestbody/App.java
@@ -0,0 +1,11 @@
+package com.demo.part14_restful_api_restcontroller_pathvariable_requestbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ part14_restful_api_restcontroller_pathvariable_requestbody/src/main/java/com/demo/part14_restful_api_restcontroller_pathvariable_requestbody/Todo.java
@@ -0,0 +1,9 @@
+package com.demo.part14_restful_api_restcontroller_pathvariable_requestbody;
+
+import lombok.Data;
+
+@Data
+public class Todo {
+    private String title;
+    private String detail;
+}
